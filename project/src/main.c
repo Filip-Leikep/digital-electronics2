fe68a1d559113eeaf003c610f3124a1b0dba7e27
Leikep Filip (230280) <xleike01@vutbr.cz>
2023-11-23 10:57:30 +0100

    eee

--- project/src/main.c
+++ project/src/main.c
@@ -1,35 +1,37 @@
 /***********************************************************************
  * 
- * The I2C (TWI) bus scanner tests all addresses and detects devices
- * that are connected to the SDA and SCL signals.
+ * Use Analog-to-digital conversion to read push buttons on LCD keypad
+ * shield and display it on LCD screen.
  * 
  * ATmega328P (Arduino Uno), 16 MHz, PlatformIO
  *
- * Copyright (c) 2023 Tomas Fryza
+ * Copyright (c) 2018 Tomas Fryza
  * Dept. of Radio Electronics, Brno University of Technology, Czechia
  * This work is licensed under the terms of the MIT license.
  * 
  **********************************************************************/
 
 
-/* Defines -----------------------------------------------------------*/
-#ifndef F_CPU
-# define F_CPU 16000000  // CPU frequency in Hz required for UART_BAUD_SELECT
-#endif
-
-
 /* Includes ----------------------------------------------------------*/
 #include <avr/io.h>         // AVR device-specific IO definitions
 #include <avr/interrupt.h>  // Interrupts standard C library for AVR-GCC
 #include "timer.h"          // Timer library for AVR-GCC
-#include <twi.h>            // I2C/TWI library for AVR-GCC
-#include <uart.h>           // Peter Fleury's UART library
+#include <uart.h>            
 #include <stdlib.h>         // C library. Needed for number conversions
-#include <oled.h>           // OLED Display
+#include <moisture_sens.h>
+#include <oled.h>
+#include <gpio.h>
 
+#define SENSOR_ADR 0x5c
+#define SENSOR_HUM_MEM 0
+#define SENSOR_TEMP_MEM 2
+#define SENSOR_CHECKSUM 4
+#define RELE PD2 
 
-/* Global variables --------------------------------------------------*/
-// Declaration of "dht12" variable with structure "DHT_values_structure"
+volatile uint8_t new_air_data = 0;
+volatile uint8_t water = 0;
+uint16_t moist_value;
+
 struct DHT_values_structure {
    uint8_t hum_int;
    uint8_t hum_dec;
@@ -38,68 +40,42 @@ struct DHT_values_structure {
    uint8_t checksum;
 } dht12;
 
-// Flag for printing new data from sensor
-volatile uint8_t new_sensor_data = 0;
-
-
-// Slave and internal addresses of temperature/humidity sensor DHT12
-#define SENSOR_ADR 0x5c
-#define SENSOR_HUM_MEM 0
-#define SENSOR_TEMP_MEM 2
-#define SENSOR_CHECKSUM 4
-
-// Adresy RTC
-#define RTC_ADR 0x68
-#define RTC_SEC 0x00 //bit7_0; bit6-4_10sec; bit3-0_sec
-#define RTC_MIN 0x01 //bit7_0; bit6-4_10min; bit3-0_min
-#define RTC_HOUR 0x02 //bit7_0; bit6_0(24h); bit5_20h; bit4_10h, bit3-0_h
-#define RTC_DATE 0x04 //bit7-6_0; bit 5-4_10date; 3-0_date
-#define RTC_MONTH 0x05 //bit7_stoleti; bit6-5_0; bit4_10month; bit 3-0_month
-#define RTC_YEAR 0x06 //bit7-4_10year; bit3-0_year
-
 /* Function definitions ----------------------------------------------*/
 /**********************************************************************
  * Function: Main function where the program execution begins
- * Purpose:  Call function to test all I2C (TWI) combinations and send
- *           detected devices to UART.
+ * Purpose:  Use Timer/Counter1 and start ADC conversion every 100 ms.
+ *           When AD conversion ends, send converted value to LCD screen.
  * Returns:  none
- * 
- * Some known devices:
- *     0x3c - OLED display
- *     0x57 - EEPROM
- *     0x5c - Temp+Humid
- *     0x68 - RTC
- *     0x68 - GY521
- *     0x76 - BME280
- *
  **********************************************************************/
-/* Function definitions ----------------------------------------------*/
-/**********************************************************************
-* Function: Main function where the program execution begins
-* Purpose:  Wait for new data from the sensor and sent them to UART.
-* Returns:  none
-**********************************************************************/
 int main(void)
 {
-    char string[2];  // String for converting numbers by itoa()
-
-    // TWI
-    twi_init();
-
-    // UART
+    // Initialize display
     uart_init(UART_BAUD_SELECT(115200, F_CPU));
+    char stringA[2];  // String for converting numbers by itoa()
+    uart_puts("test");
+    // Configure Analog-to-Digital Convertion unit
+    // Select ADC voltage reference to "AVcc with external capacitor at AREF pin"
+    moisture_sens_init(); //inicializace půdní vlhkosti
+    twi_init(); //inicializace i2c
+    // Configure 16-bit Timer/Counter1 to start ADC conversion
+    // Set prescaler to 33 ms and enable overflow interrupt
+    TIM1_OVF_1SEC
+    TIM1_OVF_ENABLE
+    // Enables interrupts by setting the global interrupt mask
+    sei();
+    GPIO_mode_output(&DDRD, RELE);
 
-    sei();  // Needed for UART
-
+    //pozice na displeji pro vzdusnou teplotu a vlhkost
     oled_init(OLED_DISP_ON);
     oled_clrscr();
     oled_charMode(NORMALSIZE);
     oled_puts("Meteostanice");
-    // oled_gotoxy(x, y)
     oled_gotoxy(0, 2);
     oled_puts("Teplota [°C]");
     oled_gotoxy(0, 3);
-    oled_puts("Vlhkost [%]");
+    oled_puts("Vlhkost RH[%]");
+    oled_gotoxy(0, 4);
+    oled_puts("Zalevani");
     oled_display();
     // Test if sensor is ready
     if (twi_test_address(SENSOR_ADR) == 0)
@@ -108,46 +84,52 @@ int main(void)
         uart_puts("[ERROR] I2C device not detected\r\n");
         while (1);
     }
-
-    // Timer1
-    TIM1_OVF_1SEC
-    TIM1_OVF_ENABLE
-
-    sei();
+    //
 
     // Infinite loop
-    while (1) {
-        if (new_sensor_data == 1) {
-            itoa(dht12.temp_int, string, 10);
+    while (1)
+    {
+        if (new_air_data == 1) {
+            itoa(dht12.temp_int, stringA, 10);
             uart_puts("T ");
-            uart_puts(string);
+            uart_puts(stringA);
             uart_puts(".");
             oled_gotoxy(15, 2);
-            oled_puts(string);
+            oled_puts(stringA);
             oled_puts(".");
-            itoa(dht12.temp_dec, string, 10);
+            itoa(dht12.temp_dec, stringA, 10);
             oled_gotoxy(18, 2);
-            oled_puts(string);
-            uart_puts(string);
+            oled_puts(stringA);
+            uart_puts(stringA);
             uart_puts(" °C\r\n");
-            itoa(dht12.hum_int, string, 10);
+            itoa(dht12.hum_int, stringA, 10);
             oled_gotoxy(15, 3);
-            oled_puts(string);
+            oled_puts(stringA);
             oled_puts(".");
             uart_puts("RH ");
-            uart_puts(string);
+            uart_puts(stringA);
             uart_puts(".");
-            itoa(dht12.hum_dec, string, 10);
-            uart_puts(string);
+            itoa(dht12.hum_dec, stringA, 10);
+            uart_puts(stringA);
             uart_puts(" %\r\n");
             uart_puts("\r\n");
             oled_gotoxy(18, 3);
-            oled_puts(string);
+            oled_puts(stringA);
+            oled_gotoxy(9, 4);
+            
+            if(moist_value>=800) {
+                oled_puts("aktivni     ");
+                GPIO_write_high(&PORTD, RELE);
+            }
+            else if(moist_value<=680){
+                oled_puts("neaktivni");
+                GPIO_write_low(&PORTD, RELE);
+            }
             oled_display();
             
             // Do not print it again and wait for the new data
-            new_sensor_data = 0;
-        }
+            new_air_data = 0;
+            }
     }
 
     // Will never reach this
@@ -157,12 +139,19 @@ int main(void)
 
 /* Interrupt service routines ----------------------------------------*/
 /**********************************************************************
-* Function: Timer/Counter1 overflow interrupt
-* Purpose:  Read temperature and humidity from DHT12, SLA = 0x5c.
-**********************************************************************/
+ * Function: Timer/Counter1 overflow interrupt
+ * Purpose:  Use single conversion mode and start conversion every 100 ms.
+ **********************************************************************/
 ISR(TIMER1_OVF_vect)
 {
-    // Test ACK from sensor
+    char string[4];  // String for converted numbers by itoa()
+    // Read converted moist_value
+    // Note that, register pair ADCH and ADCL can be read as a 16-bit moist_value ADC
+    moist_value = get_moisture();
+    // Convert "moist_value" to "string" and display it
+    itoa(moist_value, string, 10);
+    uart_puts(string);
+    uart_putc('\n');
     twi_start();
     if (twi_write((SENSOR_ADR<<1) | TWI_WRITE) == 0) {
         // Set internal memory location
@@ -176,7 +165,7 @@ ISR(TIMER1_OVF_vect)
         dht12.temp_int = twi_read(TWI_ACK);
         dht12.temp_dec = twi_read(TWI_NACK);
 
-        new_sensor_data = 1;
+        new_air_data = 1;
     }
     twi_stop();
 }
